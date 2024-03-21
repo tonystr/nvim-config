c68138d271ef8cfae53536fc1587cd69a7777c47
TonyStr <hello@tonystr.net>
2024-03-21 10:10:55 +0100

    feat: add some bindings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -586,6 +586,11 @@ require'lazy'.setup({
 		vim.keymap.set('n', '<leader>th', gitsigns.preview_hunk_inline)
 		vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
 		vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+		vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer)
+		vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk)
+		vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+		vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer)
+		vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk)
 	end },
 	{
 		'f-person/git-blame.nvim',
